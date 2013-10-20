44ce9a9b2555fda90c68a76872d86b5a584f4e09
rushil <rushil9292@gmail.com>
2013-10-20 10:44:07 +0530

    Basic frontend

--- views/layout.php
+++ views/layout.php
@@ -3,8 +3,28 @@
 		<title>
 			Syntax Error
 		</title>
+		<link href="public/css/bootstrap.css" rel="stylesheet" media="screen">
+		<link href="public/css/bootswatch.css" rel="stylesheet" media="screen">
+		<link href="public/css/docs.css" rel="stylesheet" media="screen">
 	</head>
 	<body>
-		Placeholder text
+		<header class="jumbotron masthead">
+			<div class="container">
+				<div class="row">
+					<div class="span12" style="margin:auto auto">
+						<form action="#">
+						<h1 style="font-size:60px;color:#0088cc">I'd like to know <select>
+							<option>The hindu</option>
+							<option> Times of India</option>
+							<option>Indian Express</option>
+						</select> readers' opinion about <input style="height:35px" type="text"/> </h1>
+						<button type="submit" id="submit" class="btn">Submit</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</header>
+		<div class="container">
+		</div>
 	</body>
 </html>
